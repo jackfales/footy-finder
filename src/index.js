d68fbdf20909ce3eb00b13a6a2b4d9d0ee2e29eb
Jack Fales <jfales@google.com>
2022-10-25 16:50:30 -0700

    Move firebase config to separate file

--- src/index.js
+++ src/index.js
@@ -6,20 +6,9 @@ import App from './App';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore/lite';
+import { firebaseConfig } from './firebaseConfig';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBg3DGYDndd8hLN9q79-hORDOowNyRNpT8",
-  authDomain: "footy-finder.firebaseapp.com",
-  projectId: "footy-finder",
-  storageBucket: "footy-finder.appspot.com",
-  messagingSenderId: "752279067517",
-  appId: "1:752279067517:web:3027ce1bbe4727f1cf4a14",
-  measurementId: "G-PTNV32YMHL"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
