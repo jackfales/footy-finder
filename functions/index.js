239da5e719730858e9729c11e9f30430594b79f9
Jack Fales <jfales@google.com>
2022-11-02 15:05:53 -0700

    Game preview trigger

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,21 @@
+const admin = require("firebase-admin");
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+const db = admin.firestore();
+
+exports.createGame = functions.firestore
+    .document("games/{gameId}")
+    .onCreate((snap, context) => {
+      const newGame = snap.data();
+      db.doc("games/{snap.id}/game_preview").set({
+        title: newGame.title,
+        description: newGame.description,
+        date_time: newGame.date_time,
+        players_per_team: newGame.players_per_team,
+        location: {
+          city: newGame.location.city,
+          state: newGame.location.state,
+        },
+      });
+    });
