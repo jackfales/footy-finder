26cc8487960b0780f457055409e50ae7da75bcc3
Jack Fales <jfales@google.com>
2022-11-02 15:12:03 -0700

    Fix minor createDate bug

--- src/helper.js
+++ src/helper.js
@@ -1,7 +1,7 @@
 const createDate = (date, time) => {
     const [year, month, day] = date.split("-");
     const [hour, minute] = time.split(":");
-    return new Date(year, month, day, hour, minute);
+    return new Date(year, month - 1, day, hour, minute);
 };
 
 module.exports = {
