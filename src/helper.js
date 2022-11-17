9647283b32c04a6123637a9d609d659820d7932c
Jack Fales <jfales@google.com>
2022-11-17 13:51:11 -0800

    Deploy logic

--- src/helper.js
+++ src/helper.js
@@ -1,9 +1,5 @@
-const createDate = (date, time) => {
+export const createDate = (date, time) => {
     const [year, month, day] = date.split("-");
     const [hour, minute] = time.split(":");
     return new Date(year, month - 1, day, hour, minute);
-};
-
-module.exports = {
-    createDate
 };
