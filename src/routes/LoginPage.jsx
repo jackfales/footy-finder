5c657af376920803542c3a13ea31ec6a29e24f88
Jack Fales <jfales@google.com>
2022-11-17 11:19:27 -0800

    Add styled components and center login page

--- src/routes/LoginPage.jsx
+++ src/routes/LoginPage.jsx
@@ -1,13 +1,30 @@
 import { signInWithGoogle } from '../api';
 import { Button } from 'react-bootstrap';
 
+import styled from 'styled-components';
+
+const StyledContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
 export default function LoginPage() {
   return (
-    <div>
-      <h1>Login</h1>
-      <Button variant="primary" onClick={signInWithGoogle}>
-        Sign In With Google
-      </Button>
-    </div>
+    <StyledContainer>
+      <StyledDiv>
+        <h1>Login</h1>
+        <Button variant="primary" onClick={signInWithGoogle}>
+          Sign In With Google
+        </Button>
+      </StyledDiv>
+    </StyledContainer>
   );
 }
